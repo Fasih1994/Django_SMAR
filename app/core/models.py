1f2b8f10bbbb88343f0ae7ffea2350fa03961b1c
hamza-wajeeh <hamza.wajeeh@inseyab.com>
2023-12-07 16:29:30 +0500

    Added Organization, Package, UserRole and PackageStatus Model

--- app/core/models.py
+++ app/core/models.py
@@ -29,12 +29,83 @@ class UserManager(BaseUserManager):
         return user
 
 
+class UserRole(models.Model):
+    user_role_id = models.AutoField(primary_key=True)
+    user_role_name = models.CharField(max_length=255)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    created_by = models.IntegerField(null=True, blank=True)
+    last_update_date = models.DateTimeField(auto_now=True)
+    last_updated_by = models.IntegerField(null=True, blank=True)
+    last_update_login = models.IntegerField(null=True)
+
+    def __str__(self):
+        return self.user_role_name
+
+
+class Package(models.Model):
+    package_id = models.AutoField(primary_key=True)
+    package_name = models.CharField(max_length=255)
+    package_price = models.DecimalField(max_digits=10, decimal_places=2)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    created_by = models.IntegerField(null=True, blank=True)
+    last_update_date = models.DateTimeField(auto_now=True)
+    last_updated_by = models.IntegerField(null=True, blank=True)
+    last_update_login = models.IntegerField(null=True)
+
+    def __str__(self):
+        return self.package_name
+
+
+class Organization(models.Model):
+    organization_id = models.AutoField(primary_key=True)
+    organization_name = models.CharField(max_length=255)
+    description = models.CharField(max_length=3000)
+    linkedin_profile = models.URLField()
+    industry = models.CharField(max_length=1000)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    created_by = models.IntegerField(null=True, blank=True)
+    last_update_date = models.DateTimeField(auto_now=True)
+    last_updated_by = models.IntegerField(null=True, blank=True)
+    last_update_login = models.IntegerField(null=True)
+
+    def __str__(self):
+        return self.organization_name
+
+
+class PackageStatus(models.Model):
+    organization_id = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    package_id = models.ForeignKey(Package, on_delete=models.CASCADE)
+    package_status = models.CharField(max_length=50)
+    package_start_date = models.DateField()
+    package_end_date = models.DateField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+    created_by = models.IntegerField(null=True, blank=True)
+    last_update_date = models.DateTimeField(auto_now=True)
+    last_updated_by = models.IntegerField(null=True, blank=True)
+    last_update_login = models.IntegerField(null=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['organization_id', 'package_id'],
+                                    name='package_status_id')
+        ]
+
+    def __str__(self):
+        return self.package_status
+
+
 class User(AbstractBaseUser, PermissionsMixin):
     """USER in the system"""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+
+    # user_role_id = models.ForeignKey(UserRole, on_delete=models.PROTECT)
+    # organization_id = models.ForeignKey(
+    # Organization, on_delete=models.CASCADE
+    # )
+    # package_id = models.ForeignKey(Package, on_delete=models.PROTECT)
 
     objects = UserManager()
 
