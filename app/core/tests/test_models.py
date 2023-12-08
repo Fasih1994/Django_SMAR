b9f87a1ffae4538ba496cc1641cd827f202b3d20
Aman <amanlatif1928@gmail.com>
2023-12-07 16:38:43 -0800

    added testcases for models

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -3,6 +3,7 @@
 
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from core.models import Organization, get_organization_model, Package
 
 # from core import models
 
@@ -60,3 +61,87 @@ class ModelTests(TestCase):
         )
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_check_org_model(self):
+        """test creating an organization"""            
+        organization=Organization.objects.create(
+        organization_id=101,
+        organization_name= 'my_org',
+        package_id=1,
+        package_status=1,
+        package_start_date='01/01/2020',
+        package_end_date='01/01/2024',
+        creation_date='01/01/2018',
+        created_by='oneuser',
+        last_update_date='01/01/2022',
+        last_updated_by='123',
+        last_update_login='123',
+        )   
+        organization=Organization.objects.get(id=organization.id)
+        organization_id = f'{organization.organization_id}'
+        organization_name =f'{organization.organization_name}'
+        package_id = f'{organization.package_id}'
+        package_status= f'{organization.package_status}'
+        package_start_date=f'{organization.package_start_date}'
+        package_end_date = f'{organization.package_end_date}'
+        creation_date = f'{organization.creation_date}'
+        created_by = f'{organization.created_by}'
+        last_update_date = f'{organization.last_update_date}'
+        last_updated_by = f'{organization.last_updated_by}'
+        last_update_login = f'{organization.last_update_login}'
+        self.assertEqual(organization_id,'101')
+        self.assertEqual(organization_name,'my_org')
+        self.assertEqual(package_id,1)
+        self.assertEqual(package_status, 1)
+        self.assertEqual(package_start_date,datetime.date('01/01/2020'))
+        self.assertEqual(package_end_date, datetime.date('01/01/2024'))
+        self.assertEqual(creation_date,datetime.date('01/01/2018'))
+        self.assertEqual(created_by,'oneuser')
+        self.assertEqual(last_update_date,datetime.date('01/01/2022'))
+        self.assertEqual(last_updated_by, 123)
+        self.assertEqual(last_update_login, 123)
+                
+    def tests_create_new_organization_with_orgname_and_orgid_successfull(self):
+        org_id=101
+        org_name="inseyab"
+        organization=get_organization_model().objects.create(organization_id=101, organization_name="inseyab")
+        self.assertEqual(get_organization_model().objects.all().count(),1)
+        self.assertEqual(organization.organization_id, org_id)
+        self.assertEqual(organization.organigzation_name, org_name)
+            
+            
+    # def test_no_duplicate_values_in_organization_id(self):
+    #     unique_field_name= 'organization_id'
+    #     instance1= Organization.objects.create({unique_field_name:'organization_id'})
+    #     with self.assertRaises(IntegrityError):
+    #         Organization.objects.create({unique_field_name:'organization_id'})
+                
+    def tests_package_validation(self):
+        organization=Organization.objects.create(
+        package_id = 101,
+        package_status = 1,
+        package_start_date ='1/1/2024',
+        package_end_date ='1/1/2025',
+        )
+        self.assertLess(package_start_date, package_end_date)
+        self.assertGreater(package_end_date, package_start_date)
+        self.assertEqual(package_id, 101)
+        self.assertEqual(package_status, 1)            
+        
+        
+    def tests_organization_id_dealing_with_pk(self):
+        organization = Organization.objects.create(
+            organization_id = 101
+        )
+        with self.assertRaises(IntegrityError) as same_id:
+            """create second org id with same id"""
+            organization = Organization.objects.create(
+                organization_id = 101
+            )
+        self.assertIn(organization_id,str(same_id,exception))    
+            
+                
+        
+    
+    
+    
